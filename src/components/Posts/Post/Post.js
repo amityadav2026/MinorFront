13e130377b5822e6546deb549f65630bcf5d768e
Amit Yadav <amit.yadav117019@gmail.com>
2021-10-18 17:16:59 +0530

    new

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -13,7 +13,7 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import { useDispatch } from "react-redux";
-import { deletePost } from "../../../actions/posts";
+import { deletePost, likePost } from "../../../actions/posts";
 const Post = ({ post, setCurrentId }) => {
     const dispatch = useDispatch();
     const classes = useStyles();
@@ -53,7 +53,11 @@ const Post = ({ post, setCurrentId }) => {
                 </Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => dispatch(likePost(post._id))}
+                >
                     <ThumbUpAltIcon fontSize="small" /> Like {post.likeCount}
                 </Button>
                 <Button
